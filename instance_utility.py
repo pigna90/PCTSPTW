d145de89439bb3bcb93e5cdb9fb8eb7b911b2ab7
Francesco Scigliuzzo <francesco.scigliuzzo@gmail.com>
2016-05-01 15:42:51 +0000

    function instance_loader

--- instance_utility.py
+++ instance_utility.py
@@ -65,6 +65,66 @@ def create_instance(old_instance,new_instance,m_min=0,m_max=5,p_min=1,p_max=100)
 		
 	fp_out.close()
 
+# This function return a dictionary contain the information
+# for each instance:
+# - Number of nodes
+# - Time windows for each node
+# - Price for each node
+# - Matrix of distances
+# Params:
+# path - path of the file that contain the instance
+
+
+def instance_loader(path):
+	
+	dictionary = {}
+	
+	# array contain all text file 
+	array = []
+	
+	# dictionary a contain the begin of the time windows for each node
+	a = {}
+	
+	#dictionary b contain the close of the time windows for each node
+	b = {}
+	
+	#dictionary p contain the price for each node
+	p = {}
+	
+	count = 0
+	i = 1
+	array = open(path).read().splitlines()  
+
+	# variable n contain the number of nodes
+	n = array[0]
+	while(count < int(n)):
+		
+		price = array[i].split(" ")
+		time = price[0].split(",")
+		
+		a.update({count : int(time[0])})
+		b.update({count : int(time[1])})
+		p.update({count : float(price[1])})
+		
+		count = count + 1
+		i = i + 1
+	
+	
+	i = int(n) + 1
+	
+	# create a matrix m of zero
+	m = np.zeros(shape=(int(n),int(n)))
+	
+	# populate each row of matrix 
+	for j in range(0, int(n)):
+		temp = array[i].split(" ")
+		m[j] = temp
+		i = i + 1
+	
+	dictionary = {"n": int(n), "a": a, "b": b, "p": p, "m": m}
+		
+	return dictionary
+
 def main():
 	#create_instance("Langevin Instances/N20ft204.dat","/tmp/prova.txt")
 	
