b1ac1e4f083d8caa5395924face5f0b3087f5d68
Alessandro <alerom90@gmail.com>
2016-05-07 10:10:39 +0200

    Fix origin/destination node problem

--- model.py
+++ model.py
@@ -8,17 +8,29 @@ def main():
 	ris = instance_loader("inst")
 	
 	NODES = [n for n in range(1,ris["n"])]
-	# Origin and arrive (same node)
-	d,o = [0],[0]
+	# Origin
+	o = 0
+	# Arrive
+	d = len(NODES) + 1
 	# Prizes
 	p = ris["p"]
+	p[d] = p[0]
 	# Start time windows
 	a = ris["a"]
+	a[d] = a[0]
 	# Stop time windows
 	b = ris["b"]
+	b[d] = b[0]
 	# Archs's weight
 	t = ris["m"]
+	t = np.vstack((t,t[0]))
+	t = np.concatenate((t,np.array([t[:,0]]).T),axis=1)
 
+	print(a)
+	print(b)
+	print(p)
+	print(t)
+	
 	# Maximize problem
 	prob = LpProblem("PCTSPTW",LpMaximize)
 
@@ -26,40 +38,47 @@ def main():
 	y = LpVariable.dicts("y", NODES, 0, 1, LpBinary)
 
 	# T as visit time at specific node
-	T = LpVariable.dicts("T", (d+NODES),None,None,LpInteger)
+	T = LpVariable.dicts("T", ([o]+NODES+[d]),None,None,LpInteger)
 
 	# x as decision variable for an arch between two node
-	x = LpVariable.dicts("x", [(i,j) for i in (d+NODES) for j in (d+NODES) if i!=j], 0, 1, LpBinary)
+	x = LpVariable.dicts("x", [(i,j)
+		for i in ([o]+NODES)
+		for j in (NODES+[d])
+		if i!=j], 0, 1, LpBinary)
 
+	## Print archs used
+	#for i in [o]+NODES:
+		#for j in [d]+NODES:
+			#if(i!=j):
+				#if(value(x[(i,j)]) != 1):
+					#print(x[(i,j)]," ",value(x[(i,j)]))
+	#quit()
 	# Objective function
 	prob += lpSum(p[i]*y[i] for i in NODES)
 
 	# Constraint (1)
 	for i in NODES:
-		prob += lpSum(x[(i,j)] for j in (d+NODES) if i!=j) == y[i]
+		prob += lpSum(x[(i,j)] for j in ([d]+NODES) if i!=j) == y[i]
 
 	# Constraint (2)
 	for j in NODES:
-		prob += lpSum(x[(i,j)] for i in (o+NODES) if i!= j) == y[j]
-
-	#for i in NODES:
-	#	prob += T[0] <= T[i]
+		prob += lpSum(x[(i,j)] for i in ([o]+NODES) if i!= j) == y[j]
 
 	# Constraint (3)
-	prob += lpSum(x[(0,j)] for j in NODES) == 1
+	prob += lpSum(x[(o,j)] for j in NODES) == 1
 
 	# Constraint (4)
-	prob += lpSum(x[(i,0)] for i in NODES) == 1
+	prob += lpSum(x[(i,d)] for i in NODES) == 1
 
 	# Constraint (5)
-	for i in (NODES):
-		for j in (o+NODES):
+	for i in ([o]+NODES):
+		for j in ([d]+NODES):
 			if (i != j):
 				prob += (T[i] + t[i][j] - T[j]) <= (max(b[i]+t[i][j]-a[j],0)*(1-x[(i,j)]))
 				prob += x[(i,j)] >= 0
 
 	# Constraint (6)
-	for i in (d+NODES):
+	for i in ([o]+NODES+[d]):
 		prob += T[i] >= a[i]
 		prob += T[i] <= b[i]
 
@@ -72,19 +91,22 @@ def main():
 	# Print PoI visited
 	for i in NODES:
 		if(value(y[i]) >= 1):
-			print(y[i]," ",value(y[i]))
+			print("y_" + str(i),"=",1)
 	print("--------")
 
 	# Print archs used
-	for i in d+NODES:
-		for j in d+NODES:
+	for i in [o]+NODES:
+		for j in [d]+NODES:
 			if(i!=j):
 				if(value(x[(i,j)]) >= 1):
-					print(x[(i,j)]," ",value(x[(i,j)]))
+					if j!=d:
+						print("x(" + str(i) + "_" + str(j) + ") =",1)
+					else:
+						print("x(" + str(i) + "_" + str(o) + ") =",1)
 	print("--------")
 
 	# Print T for used points
-	for i in d+NODES:
+	for i in [d]+NODES:
 		print(T[i]," ",value(T[i]))
 
 if __name__ == "__main__":
